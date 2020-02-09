50ab778a4a34daba91f9f278557c17e6090a8262
Hanlin <hanlinGao@outlook.com>
2020-02-09 08:08:09 -0500

    update

--- Client/src/app/graph/graph.component.ts
+++ Client/src/app/graph/graph.component.ts
@@ -7,13 +7,92 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class GraphComponent implements OnInit, OnDestroy {
 
-  graph = {
+  macronutrients = {
     data: [
-        { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
-        { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+      {
+      type: 'scatterpolar',
+      r: [39, 28, 8, 7, 28, 39],
+      theta: ['A','B','C', 'D', 'E', 'A'],
+      fill: 'toself',
+      name: 'Shopping list'
+      },
+      {
+      type: 'scatterpolar',
+      r: [1.5, 10, 39, 31, 15, 1.5],
+      theta: ['A','B','C', 'D', 'E', 'A'],
+      fill: 'toself',
+      name: 'Reciept'
+      }
     ],
-    layout: {width: 320, height: 240, title: 'A Fancy Plot'}
-};
+  
+    
+    layout: {
+      polar: {
+        radialaxis: {
+          visible: true,
+          range: [0, 50]
+        }
+      }
+    }
+  }
+
+    minerals = {
+      data: [
+        {
+        type: 'scatterpolar',
+        r: [39, 28, 8, 7, 28, 39],
+        theta: ['A','B','C', 'D', 'E', 'A'],
+        fill: 'toself',
+        name: 'Shopping list'
+        },
+        {
+        type: 'scatterpolar',
+        r: [1.5, 10, 39, 31, 15, 1.5],
+        theta: ['A','B','C', 'D', 'E', 'A'],
+        fill: 'toself',
+        name: 'Reciept'
+        }
+      ],
+    
+      
+      layout: {
+        polar: {
+          radialaxis: {
+            visible: true,
+            range: [0, 50]
+          }
+        }
+      }
+    }
+
+      vitamins = {
+        data: [
+          {
+          type: 'scatterpolar',
+          r: [39, 28, 8, 7, 28, 39],
+          theta: ['A','B','C', 'D', 'E', 'A'],
+          fill: 'toself',
+          name: 'Shopping list'
+          },
+          {
+          type: 'scatterpolar',
+          r: [1.5, 10, 39, 31, 15, 1.5],
+          theta: ['A','B','C', 'D', 'E', 'A'],
+          fill: 'toself',
+          name: 'Reciept'
+          }
+        ],
+      
+        
+        layout: {
+          polar: {
+            radialaxis: {
+              visible: true,
+              range: [0, 50]
+            }
+          }
+        }
+      }
 
   ngOnInit() {
 
